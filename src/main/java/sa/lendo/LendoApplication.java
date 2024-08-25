c392623cf5d786ff57467c4cdac1051aeaa8fd78
Awais Rashid <awais@192.168.1.4>
2024-08-26 01:29:31 +0500

    added 1 user in H2-DB on application startup

--- src/main/java/sa/lendo/LendoApplication.java
+++ src/main/java/sa/lendo/LendoApplication.java
@@ -1,13 +1,36 @@
 package sa.lendo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import sa.lendo.entity.User;
+import sa.lendo.repository.UserRepository;
 
 @SpringBootApplication
-public class LendoApplication {
+public class LendoApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LendoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LendoApplication.class, args);
+    }
 
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public void addUsers() {
+        var user = new User();
+        user.setUsername("bassam");
+        user.setEmail("bassam@lendo.sa");
+        user.setPassword(passwordEncoder.encode("bassam"));
+
+        userRepository.save(user);
+    }
+
+    @Override
+    public void run(String... args) {
+        addUsers();
+    }
 }
