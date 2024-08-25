c761ae4bf8dbe6253cd17d0d14a6577a2c65473b
Awais Rashid <awais@192.168.1.4>
2024-08-26 01:25:01 +0500

    added service to fetch user by username on /login

---
+++ src/main/java/sa/lendo/service/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package sa.lendo.service;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import sa.lendo.repository.UserRepository;
+
+import java.util.ArrayList;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(String.format("%s not found", username)));
+
+        return new User(user.getUsername(), user.getPassword(), new ArrayList<>());
+    }
+}
