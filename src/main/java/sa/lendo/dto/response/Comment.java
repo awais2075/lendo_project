32adc6d5c80665e7a5dd7560f47b8c1e885997a6
Awais Rashid <awais@192.168.1.4>
2024-08-26 01:22:44 +0500

    added request, response DTOs

---
+++ src/main/java/sa/lendo/dto/response/Comment.java
@@ -0,0 +1,67 @@
+package sa.lendo.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Comment {
+
+    private Long id;
+    @JsonProperty("post_id")
+    private Long postId;
+    private String name;
+    private String email;
+    private String body;
+
+    public Comment() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getPostId() {
+        return postId;
+    }
+
+    public void setPostId(Long postId) {
+        this.postId = postId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    @Override
+    public String toString() {
+        return "Comment{" +
+                "id=" + id +
+                ", postId=" + postId +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", body='" + body + '\'' +
+                '}';
+    }
+}
