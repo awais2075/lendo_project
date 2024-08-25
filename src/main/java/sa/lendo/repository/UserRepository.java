d780e5b8a7f4c498cbe314091295a96bd7299b62
Awais Rashid <awais@192.168.1.4>
2024-08-26 01:21:29 +0500

    added UserRepository

---
+++ src/main/java/sa/lendo/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package sa.lendo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import sa.lendo.entity.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
