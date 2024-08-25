31faf0beb7587813c85492ff5b65a7457bb221f0
Awais Rashid <awais@192.168.1.4>
2024-08-25 15:41:27 +0500

    initial commit

---
+++ src/main/java/sa/lendo/LendoApplication.java
@@ -0,0 +1,13 @@
+package sa.lendo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LendoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LendoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sa/lendo/LendoApplicationTests.java
@@ -0,0 +1,13 @@
+package sa.lendo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LendoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
